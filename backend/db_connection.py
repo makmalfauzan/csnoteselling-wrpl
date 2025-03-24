9937a6dd7e16f2679ea489ba7b0aab19a90fddf7
MuhPrayoga <prayogagres@gmail.com>
2025-03-24 16:54:18 +0700

    ubah isi db_connection sesuai .env

--- backend/db_connection.py
+++ backend/db_connection.py
@@ -1,9 +1,14 @@
 import mysql.connector
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 def get_db_connection():
     return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="",
-        database="noteselling-wirpl"
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME")
     )
