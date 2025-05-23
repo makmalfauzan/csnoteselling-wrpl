fca5c163d55557f9f5d21ed519fefd70fdda884a
MuhPrayoga <prayogagres@gmail.com>
2025-05-23 15:19:45 +0700

    ok

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -2,12 +2,19 @@ import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from app import create_app  # Sekarang tanpa "backend.app"
 import pytest
+from app import create_app
+from db_connection import db  # SESUAIKAN dengan letak `db` kamu
+
+from app.models import User, Wallet, Transaction  # sesuaikan
 
 @pytest.fixture
 def client():
     app = create_app(testing=True)
     with app.test_client() as client:
         with app.app_context():
+            db.session.query(Transaction).delete()
+            db.session.query(Wallet).delete()
+            db.session.query(User).delete()
+            db.session.commit()
             yield client
