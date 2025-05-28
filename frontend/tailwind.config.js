970b41e4d0352d483a3e3533333f0e9e869b34e3
MuhPrayoga <prayogagres@gmail.com>
2025-05-29 00:22:02 +0700

    homepage sdh bisa

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,14 @@
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./**/*.html",
+    "./**/*.js",
+    "./pages/**/*.html",
+    "./script/**/*.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
